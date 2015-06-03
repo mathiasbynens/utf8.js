b4433b743b9019ef8ff4de929455199e7ec2dd0d
Antelle <dmitryvit@gmail.com>
2015-06-03 12:28:47 +0300

    utf8 encoder speedup for large amounts of data processing

---
+++ tests/test-large.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// test with node.js on 50MB data encoding
+
+var utf8 = require('../utf8.js');
+console.log('started. mem: %d', mem());
+var buffer = new Buffer(1024*1024*50);
+var letters = ['a', 'ы', '6', 'ð'];
+for (var i = 0; i < buffer.length; i++) {
+    buffer[i] = letters[i % letters.length].charCodeAt(0);
+}
+var str = buffer.toString('utf8');
+buffer = null;
+console.log('created string. mem: %d', mem());
+str = utf8.encode(str);
+console.log('encoded string (%d chars). mem: %d', str.length, mem());
+str = utf8.decode(str);
+console.log('converted string (%d chars). mem: %d', str.length, mem());
+
+function mem() {
+    if (global.gc) global.gc();
+    return Math.round(process.memoryUsage().rss/1024/1024);
+}
